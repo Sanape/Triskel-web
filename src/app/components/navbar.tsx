c1f841bc32607bf26ce7603b7537440a9cf61a36
santiago <sanape.SA@GMAIL.com>
2023-06-14 16:38:10 -0300

    Initial commit

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,68 @@
+'use client';
+
+import Link from 'next/link';
+import Image from 'next/image';
+import Logo from '../../../public/img/logo-triskel limpio.png';
+import React, { useState } from 'react';
+
+export default function Navbar() {
+  const colors = ['#3cbab0', '#D19C1D', '#85C66A', '#CC5FBD', '#54167A'];
+  const [color, setColor] = useState('black');
+  const [colorBtn, setColorBtn] = useState('#D19C1D');
+  const [prevColor, setPrevColor] = useState('');
+
+  const changeColor = () => {
+    let newColor;
+    do {
+      newColor = colors[Math.floor(Math.random() * colors.length)];
+    } while (prevColor === newColor);
+    setPrevColor(newColor);
+    setColor(newColor);
+  };
+
+  const changeColorBtn = () => {
+    let newColor;
+    do {
+      newColor = colors[Math.floor(Math.random() * colors.length)];
+    } while (newColor == colorBtn);
+    setColorBtn(newColor);
+  };
+
+  return (
+    <nav className="flex items-center justify-between p-6 ">
+      <Link href="/" className={'logo'}>
+        <Image src={Logo} alt="Logo" width={60} height={60} />
+        TiskelTech +CreActivos
+      </Link>
+
+      <div className="space-x-4 justify-between flex w-6">
+        <div className={'links'}>
+          <Link
+            href="/link1"
+            className="link text-black "
+            style={{ color: color }}
+            onMouseEnter={changeColor}
+            onMouseLeave={() => setColor('black')}
+          >
+            Inicio
+          </Link>
+          <Link href="/link2" className="link text-black ">
+            Nuestra metología
+          </Link>
+          <Link href="/link3" className="link text-black ">
+            Cursos
+          </Link>
+        </div>
+        <Link
+          href="/button-link"
+          className="btn px-4 py-2 bg-white text-white rounded"
+          style={{ backgroundColor: colorBtn }}
+          onMouseEnter={changeColorBtn}
+          // onMouseLeave={() => setColorBtn('#D19C1D')}
+        >
+          Contactanos
+        </Link>
+      </div>
+    </nav>
+  );
+}
---
+++ src/app/components/timeline.tsx
@@ -0,0 +1,197 @@
+'use client';
+import Image from 'next/image';
+import React, { useEffect, useRef } from 'react';
+function Timeline() {
+  const itemsRef = useRef<HTMLLIElement[]>([]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('in-view');
+          } else {
+            entry.target.classList.remove('in-view');
+          }
+        });
+      },
+      {
+        root: null, // null means it's the viewport
+        rootMargin: '0px',
+        threshold: 0.1, // adjust this if you want the event to fire when the element is closer or farther from the viewport
+      }
+    );
+
+    const currentItems = itemsRef.current;
+    // En este código, currentItems es una constante que captura el valor actual de itemsRef.current cada vez que se ejecuta el efecto. Luego, en la función de limpieza, utilizamos currentItems en lugar de itemsRef.current, asegurándonos de que estamos utilizando el valor que itemsRef.current tenía en el momento en que se ejecutó el efecto.
+
+    currentItems.forEach((item) => {
+      observer.observe(item);
+    });
+
+    return () => {
+      currentItems.forEach((item) => {
+        observer.unobserve(item);
+      });
+    };
+  }, []);
+
+  const addToRefs = (el: HTMLLIElement) => {
+    if (el && !itemsRef.current.includes(el)) {
+      itemsRef.current.push(el);
+    }
+  };
+
+  return (
+    <section className="timeline">
+      <ul>
+        <li ref={addToRefs}>
+          {''}
+          <div>
+            <time className="time1">
+              <strong>S</strong>cience (Ciencia)
+            </time>
+            <div className="discovery">
+              <Image
+                src={'/img/Ciencia-STEAM.png'}
+                alt={'Logo Ciencia'}
+                width="200"
+                height="200"
+              ></Image>
+            </div>
+            <div className="scientist">
+              <span>
+                Desempeña un papel vital en la metodología STEAM ya que es la
+                base para comprender el mundo que nos rodea. La ciencia nos
+                proporciona el conocimiento y la comprensión de los fenómenos
+                naturales y de cómo funciona el universo, desde los principios
+                fundamentales de la física hasta la complejidad de los sistemas
+                biológicos. Con una base sólida en ciencia, los estudiantes
+                pueden desarrollar un pensamiento crítico y analítico que les
+                permitirá entender y resolver problemas complejos, fomentando
+                así su creatividad e innovación en áreas científicas y
+                tecnológicas.
+              </span>
+            </div>
+          </div>
+        </li>
+        <li ref={addToRefs}>
+          {' '}
+          <div>
+            <time className="time2">
+              <strong>T</strong>echnology (Tecnología)
+            </time>
+            <div className="discovery">
+              <Image
+                src={'/img/Tecnologia-STEAM.png'}
+                alt={'Logo Tecnología'}
+                width="200"
+                height="200"
+              ></Image>
+            </div>
+            <div className="scientist">
+              <span>
+                Crucial en la educación ya que es una herramienta que permite
+                implementar y aplicar los conocimientos adquiridos en ciencia.
+                La tecnología también ofrece nuevas formas de aprender, enseñar
+                y comunicarse, proporcionando un acceso sin precedentes a la
+                información y a las oportunidades de aprendizaje. Al dominar las
+                habilidades tecnológicas, los estudiantes pueden adaptarse a un
+                mundo que está en constante cambio y en el que la tecnología
+                juega un papel cada vez más importante.
+              </span>
+            </div>
+          </div>
+        </li>
+        <li ref={addToRefs}>
+          {''}
+          <div>
+            <time className="time3">
+              <strong>E</strong>ngineering (Ingeniería)
+            </time>
+            <div className="discovery">
+              <Image
+                src={'/img/Ingenieria-STEAM.png'}
+                alt={'Logo Ingeniería'}
+                width="200"
+                height="200"
+              ></Image>
+            </div>
+            <div className="scientist">
+              <span>
+                Esencial en la educación STEAM porque proporciona el conjunto de
+                habilidades necesario para diseñar y construir soluciones a
+                problemas reales. La ingeniería combina los principios de la
+                ciencia y la matemática con la práctica de diseño para crear
+                soluciones innovadoras. Al aprender sobre ingeniería, los
+                estudiantes no solo adquieren habilidades técnicas, sino que
+                también desarrollan habilidades de pensamiento crítico,
+                resolución de problemas y trabajo en equipo que son esenciales
+                en el siglo XXI.
+              </span>
+            </div>
+          </div>
+        </li>
+        <li ref={addToRefs}>
+          {' '}
+          <div>
+            <time className="time4">
+              <strong>A</strong>rt (Arte)
+            </time>
+            <div className="discovery">
+              <Image
+                src={'/img/Arte-STEAM.png'}
+                alt={'Logo Arte'}
+                width="200"
+                height="200"
+              ></Image>
+            </div>
+            <div className="scientist">
+              <span>
+                Componente clave de la educación STEAM, ya que fomentan la
+                creatividad, la expresión personal y la innovación. Las artes
+                pueden ayudar a los estudiantes a ver las cosas desde diferentes
+                perspectivas, a pensar fuera de la caja y a encontrar soluciones
+                creativas a los problemas. Además, las artes pueden mejorar la
+                motivación, el compromiso y el disfrute del aprendizaje, y
+                pueden proporcionar una forma de conectar la ciencia, la
+                tecnología, la ingeniería y las matemáticas con las experiencias
+                de vida de los estudiantes.
+              </span>
+            </div>
+          </div>
+        </li>
+        <li ref={addToRefs}>
+          {' '}
+          <div>
+            <time className="time5">
+              <strong>M</strong>athematics (Matemática)
+            </time>
+            <div className="discovery">
+              <Image
+                src={'/img/Matematicas-STEAM.png'}
+                alt={'Logo Matemática'}
+                width="200"
+                height="200"
+              ></Image>
+            </div>
+            <div className="scientist">
+              <span>
+                Fundamentales en la educación STEAM ya que son la base para
+                entender y aplicar conceptos científicos y tecnológicos. Las
+                matemáticas ayudan a los estudiantes a desarrollar habilidades
+                de pensamiento lógico y crítico, a resolver problemas y a tomar
+                decisiones basadas en datos. Además, el dominio de las
+                matemáticas es esencial para muchas carreras en ciencia,
+                tecnología e ingeniería.
+              </span>
+            </div>
+          </div>
+        </li>
+        {/* more li elements */}
+      </ul>
+    </section>
+  );
+}
+
+export default Timeline;
