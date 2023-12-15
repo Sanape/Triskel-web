641f04163ad3cdb56b99cc7802f8de0f1d46cec2
santiago <sanape.SA@GMAIL.com>
2023-12-15 01:23:59 -0300

    timeline refactor from plain html to mapping of  external data json array.

--- src/app/components/timeline.tsx
+++ src/app/components/timeline.tsx
@@ -1,6 +1,7 @@
 'use client';
 import Image from 'next/image';
 import React, { useEffect, useRef } from 'react';
+import { Steam } from '@/app/data.js';
 
 function Timeline() {
   const itemsRef = useRef<HTMLLIElement[]>([]);
@@ -43,55 +44,18 @@ function Timeline() {
     }
   };
 
-  const data = [
-    {
-      timeClassName: 'time1',
-      title: 'Science (Ciencia)',
-      imgSrc: '/img/Ciencia-STEAM.png',
-      imgAlt: 'Logo Ciencia',
-      desc: 'Con una base sólida en ciencia, los estudiantes pueden desarrollar un pensamiento crítico y analítico que les permitirá entender y resolver problemas complejos, fomentando así su creatividad e innovación en áreas científicas y tecnológicas.',
-    },
-    {
-      timeClassName: 'time2',
-      title: 'Technology (Tecnología)',
-      imgSrc: '/img/Tecnologia-STEAM.png',
-      imgAlt: 'Logo Tecnología',
-      desc: 'Al dominar las habilidades tecnológicas, los estudiantes pueden adaptarse a un mundo que está en constante cambio y en el que la tecnología juega un papel cada vez más importante.',
-    },
-    {
-      timeClassName: 'time3',
-      title: 'Engineering (Ingeniería)',
-      imgSrc: '/img/Ingenieria-STEAM.png',
-      imgAlt: 'Logo Ingeniería',
-      desc: 'Esencial en la educación STEAM porque proporciona el conjunto de habilidades necesario para diseñar y construir soluciones a problemas reales. La ingeniería combina los principios de la ciencia y la matemática con la práctica de diseño para crear soluciones innovadoras.',
-    },
-    {
-      timeClassName: 'time4',
-      title: 'Art (Arte)',
-      imgSrc: '/img/Arte-STEAM.png',
-      imgAlt: 'Logo Arte',
-      desc: 'La expresión artistica ayuda a los estudiantes a ver las cosas desde diferentes perspectivas, a pensar fuera de la caja y a encontrar soluciones creativas a los problemas además de mejorar el aprendizaje a través de sus propias experiencias.',
-    },
-    {
-      timeClassName: 'time5',
-      title: 'Mathematics (Matemática)',
-      imgSrc: '/img/Matematicas-STEAM.png',
-      imgAlt: 'Logo Matemática',
-      desc: 'Las matemáticas ayudan a los estudiantes a desarrollar habilidades de pensamiento lógico y crítico, a resolver problemas y a tomar decisiones basadas en datos. Además, el dominio de las matemáticas es esencial para muchas carreras en ciencia, tecnología e ingeniería.',
-    },
-  ];
   return (
     <section className="timeline">
       {/* //TODO: Convertir en ejercicio para probar Maquetadores */}
       <ul>
-        {data.map((item, index) => (
+        {Steam.map((item, index) => (
           <li key={index} ref={addToRefs}>
             <div>
               <time className={item.timeClassName}>
                 <strong>{item.title.charAt(0)}</strong>
                 {item.title.slice(1)}
               </time>
-              <div className="discovery">
+              <div className="image">
                 <Image
                   src={item.imgSrc}
                   alt={item.imgAlt}
@@ -99,7 +63,7 @@ function Timeline() {
                   height="200"
                 ></Image>
               </div>
-              <div className="scientist">
+              <div className="text">
                 <span>{item.desc}</span>
               </div>
             </div>
--- src/app/data.js
+++ src/app/data.js
@@ -1,54 +1,99 @@
 const Cursos = [
-    {
-        id: 1,
-        name: 'Introducción a la tecnología',
-        profesor: 'Lorena Mazza',
-        classes:[{
-            day: 'Martes',
-            hourStart:'16:00',
-            hourEnd: '18:00',
-        },
-        {
-            day: 'Martes',
-            hourStart: '18:00',
-            hourEnd: '20:00',
-        }
+  {
+    id: 1,
+    name: 'Introducción a la tecnología',
+    profesor: 'Lorena Mazza',
+    classes: [
+      {
+        day: 'Martes',
+        hourStart: '16:00',
+        hourEnd: '18:00',
+      },
+      {
+        day: 'Martes',
+        hourStart: '18:00',
+        hourEnd: '20:00',
+      },
     ],
-        image:'/../public/img/vuelta al mundo.jpg',
-    },
-    {
-        id: 2,
-        name: 'Robótica I',
-        profesor: 'Lorena Mazza',
-        classes:[{
-            day: 'Viernes',
-            hourStart: '16:00',
-            hourEnd: '18:00',
-        }],
-        image:'/../public/img/vuelta al mundo.jpg',
-    },
-    {
-        id: 3,
-        name: 'Robótica avanzada',
-        profesor: 'Ariel Airoldi',
-        classes:[{
-            day: 'Miércoles',
-            hourStart: '18:00',
-            hourEnd: '20:00',
-        }],
-        image:'/../public/img/arduino.jpg',
-    },
-    {
-        id: 4,
-        name: 'Programación con videojuegos',
-        profesor: 'Santiago Apesteguía',
-        classes:[{
-            day: 'Jueves',
-            hourStart: '18:00',
-            hourEnd: '20:00',
-        }],
-        image:'/../public/img/programacion.jpg',
-    }
-]
+    image: '/../public/img/vuelta al mundo.jpg',
+  },
+  {
+    id: 2,
+    name: 'Robótica I',
+    profesor: 'Lorena Mazza',
+    classes: [
+      {
+        day: 'Viernes',
+        hourStart: '16:00',
+        hourEnd: '18:00',
+      },
+    ],
+    image: '/../public/img/vuelta al mundo.jpg',
+  },
+  {
+    id: 3,
+    name: 'Robótica avanzada',
+    profesor: 'Ariel Airoldi',
+    classes: [
+      {
+        day: 'Miércoles',
+        hourStart: '18:00',
+        hourEnd: '20:00',
+      },
+    ],
+    image: '/../public/img/arduino.jpg',
+  },
+  {
+    id: 4,
+    name: 'Programación con videojuegos',
+    profesor: 'Santiago Apesteguía',
+    classes: [
+      {
+        day: 'Jueves',
+        hourStart: '18:00',
+        hourEnd: '20:00',
+      },
+    ],
+    image: '/../public/img/programacion.jpg',
+  },
+];
 
-export {Cursos}
+const Steam = [
+  {
+    timeClassName: 'time1',
+    title: 'Science (Ciencia)',
+    imgSrc: '/img/Ciencia-STEAM.png',
+    imgAlt: 'Logo Ciencia',
+    desc: 'Con una base sólida en ciencia, los estudiantes pueden desarrollar un pensamiento crítico y analítico que les permitirá entender y resolver problemas complejos, fomentando así su creatividad e innovación en áreas científicas y tecnológicas.',
+  },
+  {
+    timeClassName: 'time2',
+    title: 'Technology (Tecnología)',
+    imgSrc: '/img/Tecnologia-STEAM.png',
+    imgAlt: 'Logo Tecnología',
+    desc: 'Al dominar las habilidades tecnológicas, los estudiantes pueden adaptarse a un mundo que está en constante cambio y en el que la tecnología juega un papel cada vez más importante.',
+  },
+  {
+    timeClassName: 'time3',
+    title: 'Engineering (Ingeniería)',
+    imgSrc: '/img/Ingenieria-STEAM.png',
+    imgAlt: 'Logo Ingeniería',
+    desc: 'Esencial en la educación STEAM porque proporciona el conjunto de habilidades necesario para diseñar y construir soluciones a problemas reales. La ingeniería combina los principios de la ciencia y la matemática con la práctica de diseño para crear soluciones innovadoras.',
+  },
+  {
+    timeClassName: 'time4',
+    title: 'Art (Arte)',
+    imgSrc: '/img/Arte-STEAM.png',
+    imgAlt: 'Logo Arte',
+    desc: 'La expresión artistica ayuda a los estudiantes a ver las cosas desde diferentes perspectivas, a pensar fuera de la caja y a encontrar soluciones creativas a los problemas además de mejorar el aprendizaje a través de sus propias experiencias.',
+  },
+  {
+    timeClassName: 'time5',
+    title: 'Mathematics (Matemática)',
+    imgSrc: '/img/Matematicas-STEAM.png',
+    imgAlt: 'Logo Matemática',
+    desc: 'Las matemáticas ayudan a los estudiantes a desarrollar habilidades de pensamiento lógico y crítico, a resolver problemas y a tomar decisiones basadas en datos. Además, el dominio de las matemáticas es esencial para muchas carreras en ciencia, tecnología e ingeniería.',
+  },
+];
+
+export { Cursos, Steam };
